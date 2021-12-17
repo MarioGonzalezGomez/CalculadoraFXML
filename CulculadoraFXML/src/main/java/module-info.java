ab26dd26f0d844b5a82c8125f7309dd6cd8b75bc
MarioGonzalezGomez <80425131+MarioGonzalezGomez@users.noreply.github.com>
2021-12-17 10:07:34 +0100

    Delete CulculadoraFXML directory

--- CulculadoraFXML/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module mggcode.culculadorafxml {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens mggcode.culculadorafxml to javafx.fxml;
-    exports mggcode.culculadorafxml;
-}
